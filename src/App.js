0066e332124b365429382c6dbe829860b9f619c9
Kartik <kartikanandwho@gmail.com>
2022-07-06 22:33:55 -0700

    improved readability

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Navbar from './navbar.js'
-import MainNavbar from './mainnavbar.js'
-import FirstGrid from './first_grid.js';
+import Navbar from './navigation/navbar.js'
+import MainNavbar from './navigation/mainnavbar.js'
+import FirstGrid from './body/first_grid.js';
 import SlidingCarousel from './slidingcarousel/carousel.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
