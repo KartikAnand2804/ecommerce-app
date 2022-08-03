1c5e21db01e71e615d34c9d03fbb129833a8dc87
Daravi Joshi <daravijoshi123@gmail.com>
2022-08-03 09:59:14 +0530

    added buy now to second grid

--- src/body/second_grid.js
+++ src/body/second_grid.js
@@ -1,6 +1,8 @@
 import { display, margin, width } from '@mui/system';
 import React from 'react';
+import { Figure } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
+import { Link } from 'react-router-dom';
 
 
 
@@ -8,41 +10,55 @@ import Carousel from 'react-bootstrap/Carousel';
 export default function SecondGrid(){
     return(
         <>
-        <div className="carousel" style={{margin: '0 auto',width: '50vw'}}>
+        <div className="carousel"  style={{margin: '0 auto',width: '50vw'}}>
 
         <Carousel style={{display:'inline-block', width: '50vw'}}>
         <Carousel.Item style={{display: 'inline-block', width:'50vw'}}>
-        <img
-         className="d-block" style={{width:'50vw'}}
-        src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/carousel-tile/nord-ce-l.jpg.transform/scale-50/image.jpg"
-        alt="First slide"
-        />
+        <figure className='position-relative'>
+            <Link to="/cart"><img className="d-block img-fluid" style={{width:'50vw'}} src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/carousel-tile/TV-U1S-l.jpg.transform/scale-50/image.jpg" alt="First slide"/></Link>
+                <figcaption>
+                <Link to="/cart" class="text-amber-800 no-underline text-xl mb-5 ml-3 font-semibold">Buy Now</Link>
+                </figcaption>
+            </figure>
         </Carousel.Item>
         <Carousel.Item>
-        <img
-        className="d-block" style={{width:'50vw'}}
-        src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/carousel-tile/TV-U1S-l.jpg.transform/scale-50/image.jpg"
-        alt="Second slide"
-        />
+        <figure className='position-relative'>
+            <Link to="/cart"><img className="d-block img-fluid" style={{width:'50vw'}} src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/carousel-tile/Nord-Buds-Blue-L.jpg.transform/scale-50/image.jpg" alt="Second slide"/></Link>
+                <figcaption>
+                <Link to="/cart" class="text-lime-700 no-underline text-xl ml-4 font-semibold">Buy Now</Link>
+                </figcaption>
+            </figure>
         </Carousel.Item>
         <Carousel.Item>
-        <img
-        className="d-block"style={{width:'50vw'}}
-        src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/carousel-tile/Nord-Buds-Blue-L.jpg.transform/scale-50/image.jpg"
-        alt="Third slide"
-        />
+        <figure className='position-relative'>
+            <Link to="/cart"><img className="d-block img-fluid" style={{width:'50vw'}} src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/carousel-tile/nord-ce-l.jpg.transform/scale-50/image.jpg" alt="Third slide"/></Link>
+                <figcaption>
+                <Link to="/cart" class="text-black no-underline text-xl ml-3 font-bold">Buy Now</Link>
+                </figcaption>
+            </figure>
         </Carousel.Item>
         </Carousel>
         </div>
         <div className ="secondgrid"style={{ margin: '0 auto',width: '50vw'}}>
 
+            
             <div className="container4" style={{display: 'inline-block',width: '25vw'}}>
-            <img alt="timer" style={{display: 'inline-block',width: '25vw'}} src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/tile/watch-l.jpg"/>
+            <figure className='position-relative'>
+            <Link to="/cart"><img alt="timer" style={{display: 'inline-block',width: '25vw'}} src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/tile/watch-l.jpg"/></Link>
+            <figcaption>
+                <Link to="/cart" class="text-white no-underline text-xl mr-10 font-bold">Buy Now</Link>
+                </figcaption>
+            </figure>
             </div>
             
             
             <div className="container5" style={{display: 'inline-block',width: '25vw'}}>
-            <img alt="timer" style={{display: 'inline-block',width: '25vw'}} src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/tile/BWZ2-Red-L.jpg"/>
+            <figure className='position-relative'>
+            <Link to="/cart"><img alt="timer" style={{display: 'inline-block',width: '25vw'}} src="https://oasis.opstatics.com/content/dam/oasis/page/homepage/new-in/tile/BWZ2-Red-L.jpg"/></Link>
+            <figcaption>
+                <Link to="/cart" class="text-black no-underline text-xl ml-3 font-bold">Buy Now</Link>
+                </figcaption>
+            </figure>
           
             </div>
             
